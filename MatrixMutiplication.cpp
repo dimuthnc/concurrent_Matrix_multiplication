e70bac0d192ef502af2e81746a0e014dcfc5e4f9
Dimuth Menikgama <dimuthu.13@cse.mrt.ac.lk>
2017-07-27 20:26:46 +0530

    print details function changed to print a description

--- MatrixMutiplication.cpp
+++ MatrixMutiplication.cpp
@@ -14,15 +14,17 @@ using namespace std;
 double sampleSize = 20;
 struct timeval startTime, endTime;
 
-void printResults(int matrix_size, double totalTime, double sqSum)
+void printResults(int matrix_size, double totalTime, double sqSum,char* description)
 {
 
-    double average = totalTime / sampleSize;                     //get average execution time
-    double std = sqrt((sqSum / sampleSize) - (average * average)); //calculate STD
-    double n = pow(((100 * 1.96 * std) / (5 * average)), 2);      //calcuate number of samples
+    double average = totalTime / sampleSize;
+    double std = sqrt((sqSum / sampleSize) - (average * average));
+    double n = pow(((100 * 1.96 * std) / (5 * average)), 2);
 
-    printf("\n \n \n \n" );
+    printf("\n \n" );
 
+    printf(description);
+    printf("\n");
     printf("Matrix size of %d * %d \n",matrix_size, matrix_size);
     printf("Average of results %f \n",average);
     printf("Standard Deviation of results %f \n",std);
@@ -64,23 +66,21 @@ void matrixMultiplicationOptimized(int size){
         }
     }
 
-    //transpose the matrixB
+    //MATRIXB TRANSPOSE CALCULATION
     for (int i = 0; i < size; i++){
         for (int j = 0; j < size; j++){
             transposed_matrixB[j][i] = matrixB[i][j];
         }
     }
+    //FIRST OPTIMIZATION (tAKING THE TRANSPOSE OF MATRIXB)
 
-    //define variables for get execution time
     double totalTime = 0, sqTime = 0;
 
-    //loop till sample size for each matrice
     for (int count = 0; count < sampleSize; count++){
 
-        // double begin = omp_get_wtime();         //get start time
         gettimeofday(&startTime, NULL);
-//run the execution parallely
-#pragma omp parallel for
+
+        #pragma omp parallel for
         for (int i = 0; i < size; i++){
             for (int j = 0; j < size; j++){
                 double tot = 0;
@@ -90,12 +90,8 @@ void matrixMultiplicationOptimized(int size){
                 matrixC[i][j] = tot; //set value to matrixC
             }
         }
-        if (count == 0){
-            printf("Answer from version 1: %f\n", matrixC[1][1]);
-        }
 
-        // double end = omp_get_wtime();               //get end time
-        // double time_elapsed = (end - begin);        //execution time
+
 
         gettimeofday(&endTime, NULL);
         double time_elapsed = ((endTime.tv_sec - startTime.tv_sec) * 1000000u +
@@ -104,25 +100,20 @@ void matrixMultiplicationOptimized(int size){
         totalTime += time_elapsed;
         sqTime += (time_elapsed * time_elapsed);
     }
-    printResults(size, totalTime, sqTime); //find sample size,average,STD
-
-    /*
-     * from here we implement more optimised version
-     */
-    //loop till sample size for each matrice
+    printResults(size, totalTime, sqTime,"First Optimization Results"); //find sample size,average,STD
+    //SECOND OPTIMIZATION(TILED METHOD)
     setMatrixToZero(matrixC, size);
     totalTime = 0, sqTime = 0;
     for (int count=0; count<sampleSize; count++){
-        // double begin = omp_get_wtime();
-        //run execute parallely
+
         int thread_count = 20;
         int step = size/thread_count;
-        //variables for iteration
+
         int i,j,k,a,b,c;
         double tot;
-        //get start time
+
         gettimeofday(&startTime, NULL);
-#pragma omp parallel for shared(matrixA, matrixB, matrixC) private(i, j, k, tot) schedule(static) num_threads(thread_count)
+        #pragma omp parallel for shared(matrixA, matrixB, matrixC) private(i, j, k, tot) schedule(static) num_threads(thread_count)
         for (a = 0; a < size; a += step){
             for (b = 0; b < size; b += step){
                 for (c = 0; c < size; c += step){
@@ -133,7 +124,7 @@ void matrixMultiplicationOptimized(int size){
                             for (k = c; k < min(size, c + step); k++){
                                 tot += matrixA[i][k] * transposed_matrixB[j][k]; //get local total
                             }
-#pragma omp critical
+        #pragma omp critical
                             matrixC[i][j] += tot; //set value to matrixC
                         }
                     }
@@ -141,17 +132,15 @@ void matrixMultiplicationOptimized(int size){
             }
         }
         gettimeofday(&endTime, NULL);
-        if (count == 0){
-            printf("Answer from version 2: %f\n", matrixC[1][1]);
-        }
+
         double time_elapsed = ((endTime.tv_sec - startTime.tv_sec) * 1000000u +
                                endTime.tv_usec - startTime.tv_usec) /
                               1.e6;
         totalTime += time_elapsed;
         sqTime += (time_elapsed * time_elapsed);
     }
-    printf("more optimized version\n");
-    printResults(size, totalTime, sqTime); //find sample size,average,STD
+    
+    printResults(size, totalTime, sqTime,"First Optimization Results"); //find sample size,average,STD
 
     //free the allocated memory
     free(matrixA);
@@ -161,17 +150,13 @@ void matrixMultiplicationOptimized(int size){
 }
 
 void matrixMultiplicationSequential(int size){
-    /*
-    Each vector will hold the matrix in a 2D vector space
-    */
+
     vector<vector<double> > matrixA(size, vector<double>(size));
     vector<vector<double> > matrixB(size, vector<double>(size));
     vector<vector<double> > matrixC(size, vector<double>(size));
 
-    for (int i = 0; i < size; i++)
-    {
-        for (int j = 0; j < size; j++)
-        {
+    for (int i = 0; i < size; i++){
+        for (int j = 0; j < size; j++){
             matrixA[i][j] = rand() % 100000;
             matrixB[i][j] = rand() % 100000;
         }
@@ -179,17 +164,12 @@ void matrixMultiplicationSequential(int size){
 
     double totalTime = 0, sqTime = 0;
 
-    for (int count = 0; count < sampleSize; count++)
-    {
-
+    for (int count = 0; count < sampleSize; count++){
         gettimeofday(&startTime, NULL);
-        for (int i = 0; i < size; i++)
-        {
-            for (int j = 0; j < size; j++)
-            {
+        for (int i = 0; i < size; i++){
+            for (int j = 0; j < size; j++){
                 matrixC[i][j] = 0;
-                for (int k = 0; k < size; k++)
-                {
+                for (int k = 0; k < size; k++){
                     matrixC[i][j] += matrixA[i][k] * matrixB[k][j];
                 }
             }
@@ -202,22 +182,17 @@ void matrixMultiplicationSequential(int size){
         totalTime += time_elapsed;
     }
 
-    printResults(size,totalTime,sqTime);
-    //printf("Time taken for matrix size of %d %d is %f \n",size,size,(totalTime/noOfSamples));
+    printResults(size,totalTime,sqTime,"Sequential Results");
 
 }
 void matrixMultiplicationParallel(int size){
-    /*
-    Each vector will hold the matrix in a 2D vector space
-    */
+
     vector<vector<double> > matrixA(size, vector<double>(size));
     vector<vector<double> > matrixB(size, vector<double>(size));
     vector<vector<double> > matrixC(size, vector<double>(size));
 
-    for (int i = 0; i < size; i++)
-    {
-        for (int j = 0; j < size; j++)
-        {
+    for (int i = 0; i < size; i++){
+        for (int j = 0; j < size; j++){
             matrixA[i][j] = rand() % 100000;
             matrixB[i][j] = rand() % 100000;
         }
@@ -225,18 +200,14 @@ void matrixMultiplicationParallel(int size){
 
     double totalTime = 0, sqTime = 0;
 
-    for (int count = 0; count < sampleSize; count++)
-    {
+    for (int count = 0; count < sampleSize; count++){
 
         gettimeofday(&startTime, NULL);
         #pragma omp parallel for
-        for (int i = 0; i < size; i++)
-        {
-            for (int j = 0; j < size; j++)
-            {
+        for (int i = 0; i < size; i++){
+            for (int j = 0; j < size; j++){
                 matrixC[i][j] = 0;
-                for (int k = 0; k < size; k++)
-                {
+                for (int k = 0; k < size; k++){
                     matrixC[i][j] += matrixA[i][k] * matrixB[k][j];
                 }
             }
@@ -249,8 +220,7 @@ void matrixMultiplicationParallel(int size){
         totalTime += time_elapsed;
     }
 
-    printResults(size,totalTime,sqTime);
-    //printf("Time taken for matrix size of %d %d is %f \n",size,size,(totalTime/noOfSamples));
+    printResults(size,totalTime,sqTime,"Parallel Results");
 
 }
 
@@ -258,11 +228,10 @@ void matrixMultiplicationParallel(int size){
 
 int main(int argc, char **argv)
 {
-    for (int i = 200; i <= 2000; i += 200)
-    {
-        matrixMultiplicationSequential(i);
-        matrixMultiplicationParallel(i);
-        matrixMultiplicationOptimized(i);
+    for (int matSize = 200; matSize <= 200;matSize += 200){
+        matrixMultiplicationSequential(matSize);
+        matrixMultiplicationParallel(matSize);
+        matrixMultiplicationOptimized(matSize);
     }
     return 0;
 }
